0e22eba8a93f964f109209db1014c6dc03c9baae
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-05-04 21:46:01 +0400

    Updated changes

--- server/database.py
+++ server/database.py
@@ -7,7 +7,9 @@ from sqlalchemy.orm import sessionmaker
 # SB SERVER IP 192.168.100.21
 # DB_URL = "mysql+pymysql://qqdb_wweb:M6p8xK7q1E@192.168.100.21:3306/schichtprotokoll"
 # DB_URL = "mysql+pymysql://root@127.0.0.1:3306/mex"
-DB_URL = "mysql+pymysql://root:admin31@34.68.18.145:3306/schichtprotokoll"
+# DB_URL = "mysql+pymysql://root:admin31@34.68.18.145:3306/schichtprotokoll"
+# DB_URL = "mysql+pymysql://root:admin31@127.0.0.1:3306/schichtprotokoll"
+DB_URL = "mysql+pymysql://root@127.0.0.1:3306/schichtprotokoll"
 engine = create_engine(DB_URL,echo=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
