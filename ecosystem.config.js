be9876b53ef1354166ae72b44932b8f3ed5ac399
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-05-06 16:25:05 +0400

    Updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,17 @@
 
-// pm2 start "uvicorn server.main:app --port=3231" --name "server-nfc"
-module.exports = {
-  apps : [{
-    name: "server-nfc",
-    args: "uvicorn server.main:app --port=7878",
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production'
-    },
-    interpreter: "/home/qqtechx/nfc_dash/myenv/bin/python"
-  }]
-}
+// pm2 start "uvicorn server.main:app --port=7878" --name "server-nfc"
+// module.exports = {
+//   apps : [{
+//     name: "server-nfc",
+//     script: "uvicorn",
+//     args: "uvicorn server.main:app --port=7878",
+//     instances: 1,
+//     autorestart: true,
+//     watch: false,
+//     max_memory_restart: '1G',
+//     env: {
+//       NODE_ENV: 'production'
+//     },
+//     interpreter: "/home/qqtechx/nfc_dash/myenv/bin/python"
+//   }]
+// }
