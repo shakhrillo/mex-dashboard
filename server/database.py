43a703980295ef7dd41fd456bc1e9838718dbb10
shakhrillo <mr.jhon1327@gmail.com>
2024-05-06 18:05:34 +0000

    changes

--- server/database.py
+++ server/database.py
@@ -2,15 +2,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# host="192.168.100.21",
-#   user="qqdb_wweb",
-#   password="M6p8xK7q1E",
-#   database="schichtprotokoll"
-# DB_URL = "mysql+pymysql://qqdb_wweb:M6p8xK7q1E@192.168.100.21:3306/schichtprotokoll"
-# host="127.0.0.1",
-#   user="root",
-#   database="schichtprotokoll"
-# DB_URL = "mysql+pymysql://new_user:password@127.0.0.1:3306/schichtprotokoll"
 
 host=os.getenv("DB_HOST"),
 user=os.getenv("DB_USER"),
