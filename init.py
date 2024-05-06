de157a3cb97b952aaffaf4ced08dce0b8a0f60cf
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-05-06 17:31:38 +0400

    Updates

--- init.py
+++ init.py
@@ -25,5 +25,33 @@ if len(rows) == 0:
 
 cursor.execute("CREATE TABLE IF NOT EXISTS machines (id INT AUTO_INCREMENT PRIMARY KEY, token VARCHAR(255), machineQrCode VARCHAR(255), toolMounted BOOLEAN, machineMounted BOOLEAN, barcodeProductionNo VARCHAR(255), partNumber INT, partName INT, cavity INT, cycleTime VARCHAR(255), partStatus VARCHAR(255), pieceNumber INT, note VARCHAR(255), toolCleaning VARCHAR(255), remainingProductionTime INT, operatingHours INT, machineStatus VARCHAR(255))")
 
+# {
+#     "token": "0004650166692",
+#     "machineQrCode": "F450iA–1",
+#     "toolMounted": true,
+#     "machineMounted": true,
+#     "barcodeProductionNo": "0004650166692",
+#     "partNumber": 123456,
+#     "partName": 123,
+#     "cavity": 1,
+#     "cycleTime": "2,3",
+#     "partStatus": "Good",
+#     "pieceNumber": 1,
+#     "note": "Note",
+#     "toolCleaning": "3,2",
+#     "remainingProductionTime": 0,
+#     "operatingHours": 0,
+#     "machineStatus": "completed"
+# }
+
+# add above data to machines table
+cursor.execute("SELECT * FROM machines WHERE machineQrCode = 'F450iA–1'")
+rows = cursor.fetchall()
+
+if len(rows) == 0:
+  cursor.execute("INSERT INTO machines (token, machineQrCode, toolMounted, machineMounted, barcodeProductionNo, partNumber, partName, cavity, cycleTime, partStatus, pieceNumber, note, toolCleaning, remainingProductionTime, operatingHours, machineStatus) VALUES ('0004650166692', 'F450iA–1', true, true, '0004650166692', 123456, 123, 1, '2,3', 'Good', 1, 'Note', '3,2', 0, 0, 'completed')") 
+  conn.commit()
+  print("Data inserted successfully")
+
 cursor.close()
 conn.close()
