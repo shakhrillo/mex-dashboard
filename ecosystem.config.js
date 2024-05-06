bbb5ad58e90b4dd7439529fb8728c4843ff26c2b
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-05-06 16:15:14 +0400

    Changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
-// pm2 start "uvicorn server.main:app --port=3231" --name "server-nfc"
-// http://192.168.100.23:7878
-// http://192.168.100.23:7878/api/machines/F450iA–1/status
 module.exports = {
-  apps: [
-    {
-      name: "server-nfc",
-      script: "myenv/bin/uvicorn",
-      args: "server.main:app --port=7870",
-    },
-  ],
-};
+  apps : [{
+    name: "server-nfc",
+    script: "uvicorn",
+    args: "server.main:app --port=3231",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }]
+}
