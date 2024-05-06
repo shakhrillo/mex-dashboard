77c132a5b676397cdd817be81de17b0c615a5dda
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-05-06 16:00:15 +0400

    Updates

--- server/database.py
+++ server/database.py
@@ -1,15 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# DB Name> schichtprotokoll
-# DB User> qqdb_wweb
-# DB Password> M6p8xK7q1E
-# SB SERVER IP 192.168.100.21
+# host="192.168.100.21",
+#   user="qqdb_wweb",
+#   password="M6p8xK7q1E",
+#   database="schichtprotokoll"
+DB_URL = "mysql+pymysql://qqdb_wweb:M6p8xK7q1E@192.168.100.21:3306/schichtprotokoll"
 # host="127.0.0.1",
 #   user="root",
 #   database="schichtprotokoll"
 # DB_URL = "mysql+pymysql://new_user:password@127.0.0.1:3306/schichtprotokoll"
-DB_URL = "mysql+pymysql://root@127.0.0.1:3306/schichtprotokoll"
+# DB_URL = "mysql+pymysql://root@127.0.0.1:3306/schichtprotokoll"
 engine = create_engine(DB_URL,echo=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
