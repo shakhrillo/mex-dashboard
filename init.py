8cf3e0a74d8beacdcc78e8963d8fa6bb446aab5f
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-05-07 02:54:12 +0400

    changes

--- init.py
+++ init.py
@@ -21,13 +21,14 @@ if len(rows) == 0:
   conn.commit()
 
 cursor.execute("DROP TABLE IF EXISTS data")
-cursor.execute("CREATE TABLE IF NOT EXISTS data (id INT AUTO_INCREMENT PRIMARY KEY, createdAt VARCHAR(255), shift VARCHAR(255), token VARCHAR(255), machineQrCode VARCHAR(255), toolMounted BOOLEAN, machineMounted BOOLEAN, barcodeProductionNo VARCHAR(255), cavity INT, cycleTime VARCHAR(255), partStatus VARCHAR(255), pieceNumber INT, note VARCHAR(255), toolCleaning VARCHAR(255), remainingProductionTime INT, operatingHours INT, machineStatus VARCHAR(255))")
+# Add above data to the table
+cursor.execute("CREATE TABLE IF NOT EXISTS data (id INT AUTO_INCREMENT PRIMARY KEY, createdAt VARCHAR(255), shift VARCHAR(255), token VARCHAR(255), machineQrCode VARCHAR(255), toolMounted BOOLEAN, machineMounted BOOLEAN, barcodeProductionNo VARCHAR(255), cavity INT, cycleTime INT, partStatus VARCHAR(255), pieceNumber INT, note VARCHAR(255), toolCleaning VARCHAR(255), remainingProductionTime INT, remainingProductionDays INT, operatingHours INT, machineStatus VARCHAR(255))")
 
 cursor.execute("SELECT * FROM data")
 rows = cursor.fetchall()
 
 if len(rows) == 0:
-  cursor.execute("INSERT INTO data (createdAt, shift, token, machineQrCode, toolMounted, machineMounted, barcodeProductionNo, cavity, cycleTime, partStatus, pieceNumber, note, toolCleaning, remainingProductionTime, operatingHours, machineStatus) VALUES ('2021-09-01 00:00:00', '1', '0004650166692', '0004650166692', 1, 1, '0004650166692', 1, '00:00:00', 'OK', 1, 'OK', 'OK', 0, 0, 'OK')")
+  cursor.execute("INSERT INTO data (createdAt, shift, token, machineQrCode, toolMounted, machineMounted, barcodeProductionNo, cavity, cycleTime, partStatus, pieceNumber, note, toolCleaning, remainingProductionTime, remainingProductionDays, operatingHours, machineStatus) VALUES ('2021-09-01 00:00:00', '1', '0004650166692', '0004650166692', 1, 1, '0004650166692', 1, 8, 'OK', 1, 'OK', 'OK', 0, 0, 0, 'OK')")
   conn.commit()
   print("Data inserted successfully")
 
