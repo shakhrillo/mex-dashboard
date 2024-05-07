7a489761351b08e4b1d401808dab181878cd4b1a
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-05-07 12:07:38 +0400

    changes

--- init.py
+++ init.py
@@ -10,8 +10,10 @@ conn = mysql.connector.connect(
 )
 
 cursor = conn.cursor()
-cursor.execute("DROP TABLE IF EXISTS users")
-cursor.execute("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), surname VARCHAR(255), token VARCHAR(255))")
+
+if config["DB_DEV"] == "True":
+  cursor.execute("DROP TABLE IF EXISTS users")
+  cursor.execute("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), surname VARCHAR(255), token VARCHAR(255))")
 
 cursor.execute("SELECT * FROM users WHERE surname = 'admin'")
 rows = cursor.fetchall()
@@ -20,9 +22,9 @@ if len(rows) == 0:
   cursor.execute("INSERT INTO users (name, surname, token) VALUES ('admin', 'admin', '0004650166692')")
   conn.commit()
 
-cursor.execute("DROP TABLE IF EXISTS data")
-# Add above data to the table
-cursor.execute("CREATE TABLE IF NOT EXISTS data (id INT AUTO_INCREMENT PRIMARY KEY, createdAt VARCHAR(255), shift VARCHAR(255), token VARCHAR(255), machineQrCode VARCHAR(255), toolMounted BOOLEAN, machineMounted BOOLEAN, barcodeProductionNo VARCHAR(255), cavity INT, cycleTime INT, partStatus VARCHAR(255), pieceNumber INT, note VARCHAR(255), toolCleaning VARCHAR(255), remainingProductionTime INT, remainingProductionDays INT, operatingHours INT, machineStatus VARCHAR(255))")
+if config["DB_DEV"] == "True":
+  cursor.execute("DROP TABLE IF EXISTS data")
+  cursor.execute("CREATE TABLE IF NOT EXISTS data (id INT AUTO_INCREMENT PRIMARY KEY, createdAt VARCHAR(255), shift VARCHAR(255), token VARCHAR(255), machineQrCode VARCHAR(255), toolMounted BOOLEAN, machineMounted BOOLEAN, barcodeProductionNo VARCHAR(255), cavity INT, cycleTime INT, partStatus VARCHAR(255), pieceNumber INT, note VARCHAR(255), toolCleaning VARCHAR(255), remainingProductionTime INT, remainingProductionDays INT, operatingHours INT, machineStatus VARCHAR(255))")
 
 cursor.execute("SELECT * FROM data")
 rows = cursor.fetchall()
@@ -41,19 +43,21 @@ conn2 = mysql.connector.connect(
 
 # Create a table in the database
 
-# cursor2 = conn2.cursor()
-# cursor2.execute("DROP TABLE IF EXISTS bauf")
-# cursor2.execute("CREATE TABLE IF NOT EXISTS bauf (id INT AUTO_INCREMENT PRIMARY KEY, bauf_artnr INT, bauf_artbez INT)")
-# cursor2.execute("SELECT * FROM bauf")
-# rows = cursor2.fetchall()
+cursor2 = conn2.cursor()
 
-# if len(rows) == 0:
-#   cursor2.execute("INSERT INTO bauf (bauf_artnr, bauf_artbez) VALUES (80735, 001)") 
-#   conn2.commit()
-#   print("Data inserted successfully")
+if config["DB_DEV"] == "True":
+  cursor2.execute("DROP TABLE IF EXISTS bauf")
+  cursor2.execute("CREATE TABLE IF NOT EXISTS bauf (id INT AUTO_INCREMENT PRIMARY KEY, bauf_artnr INT, bauf_artbez INT)")
+  cursor2.execute("SELECT * FROM bauf")
+  rows = cursor2.fetchall()
 
-# cursor2.close()
-# conn2.close()
+  if len(rows) == 0:
+    cursor2.execute("INSERT INTO bauf (bauf_artnr, bauf_artbez) VALUES (80735, 001)") 
+    conn2.commit()
+    print("Data inserted successfully")
+
+cursor2.close()
+conn2.close()
 
 cursor.close()
 conn.close()
