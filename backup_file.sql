edf973a443c5e8b5434ae7ca20207920adbf599c
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-05-22 11:27:35 +0400

    changes

--- backup_file.sql
+++ backup_file.sql
@@ -121,6 +121,28 @@ CREATE TABLE `machines` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+-- insert into machines
+-- E 35-1
+-- E 45-1
+-- E 45-2
+-- F450iA-1
+-- E 50-2
+-- E 50-3
+-- F150iA-1
+-- Emac50-1
+-- Emac50-2
+-- Emac50-3
+-- KM 50-1
+-- KM 80-1
+-- KM 150-1
+-- E 55-1
+-- KM 420-1
+-- E 120-1
+-- E 80-1
+-- F250iA-1
+
+INSERT INTO `machines` VALUES ('E 45-1'),('E 45-2'),('F450iA-1'),('E 50-2'),('E 50-3'),('F150iA-1'),('Emac50-1'),('Emac50-2'),('Emac50-3'),('KM 50-1'),('KM 80-1'),('KM 150-1'),('E 55-1'),('KM 420-1'),('E 120-1'),('E 80-1'),('F250iA-1');
+
 --
 -- Dumping data for table `machines`
 --
