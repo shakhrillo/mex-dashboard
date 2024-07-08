aba9f18ac4a9a8efffb4fa40eac9383260fe01b6
shakhrillo <mr.jhon1327@gmail.com>
2024-07-08 18:13:26 +0400

    changes

--- src/components/global/index.js
+++ src/components/global/index.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-    // API_URL: 'http://192.168.100.23:7878',
-    API_URL: 'http://127.0.0.1:8000',
+    API_URL: 'http://192.168.100.23:7878',
+    // API_URL: 'http://127.0.0.1:8000',
 }
 
 export const machines = [
