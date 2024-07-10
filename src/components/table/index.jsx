dd6f425d8df129e759bfa11cc947a742561c301e
shakhrillo <mr.jhon1327@gmail.com>
2024-07-10 12:15:29 +0400

    fixes

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -214,7 +214,7 @@ const Table = ({ columns, data }) => {
             _status = "success";
           }
           
-          if (data["toolNo"] && data["machineStopped"])
+          if (data["toolNo"] && data["machineStopped"]) {
             _status = "warning";
           }
 
