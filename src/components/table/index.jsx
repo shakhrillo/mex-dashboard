c6a5b93124ffd4b15621e734174fee7fc955f984
shakhrillo <mr.jhon1327@gmail.com>
2024-07-11 14:55:17 +0400

    typo fixes

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -214,7 +214,7 @@ const Table = ({ columns, data }) => {
             _status = "success";
           }
           
-          if (item["toolMounted"] && item["machineStopped"]) {
+          if (data["toolMounted"] && data["machineStopped"]) {
             _status = "warning";
           }
 
