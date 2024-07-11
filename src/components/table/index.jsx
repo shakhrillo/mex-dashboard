9ef85960c8fdd3aeb2bbba2ad1fb4baff7719972
shakhrillo <mr.jhon1327@gmail.com>
2024-07-11 14:51:04 +0400

    Updated changes

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -214,7 +214,7 @@ const Table = ({ columns, data }) => {
             _status = "success";
           }
           
-          if (data["toolNo"] && data["machineStopped"]) {
+          if (item["toolMounted"] && item["machineStopped"]) {
             _status = "warning";
           }
 
@@ -356,7 +356,7 @@ const Table = ({ columns, data }) => {
                     _status = "success";
                   }
                   
-                  if (item["partStatus"] && item["machineStopped"]) {
+                  if (item["toolMounted"] && item["machineStopped"]) {
                     _status = "warning";
                   }
 
